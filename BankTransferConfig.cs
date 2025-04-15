de861a21ddbb7800a3d2744fea05846976a8b284
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-15 08:19:19 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022300043
+{
+    public class BankTransferConfig
+    {
+        private const string ConfigPath = "bank_transfer_config.json";
+
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public List<string> methods {  get; set; } 
+        public Confirmation confirmation { get; set; }
+
+
+        public class Transfer
+        {
+            public int threshold { get; set; }
+            public int low_fee { get; set; } 
+            public int high_fee { get; set; } 
+        }
+
+        public class Confirmation
+        {
+            public string en { get; set; } = "yes";
+            public string id { get; set; } = "ya";
+        }
+
+
+        public void setDefault()
+        {
+            BankTransferConfig bank = new BankTransferConfig();
+            bank.lang = "en";
+            bank.transfer.threshold = 25000000;
+            bank.transfer.low_fee = 6500;
+            bank.transfer.high_fee = 15000;
+            bank.methods = ["RTO(real-time)", "SKIN", "RTGS", "BI FAST"];
+            bank.confirmation.en = "yes";
+            bank.confirmation.id = "ya";
+        }
+
+        public BankTransferConfig LoadConfig()
+        {  
+            string json = File.ReadAllText(ConfigPath);
+            var data = JsonSerializer.Deserialize<BankTransferConfig>(json);return data;
+        }
+
+        public void SaveConfig()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(this, options);
+            File.WriteAllText(ConfigPath, json);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using modul8_103022300043;
+
+class Program
+{
+    static void Main()
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        BankTransferConfig data = config.LoadConfig();
+        //Console.WriteLine($"{data.lang}");
+        if (data.lang == "en")
+        {
+            Console.Write("Please insert the amount of money to transfer:");
+        }
+        else if (data.lang == "id")
+        {
+            Console.Write("Masukkan jumlah uang yang akan di-transfer: ");
+        }
+
+        int biayaTransfer;
+        int jumlahUang = Convert.ToInt32(Console.ReadLine());
+        int totalBiaya;
+
+        if (jumlahUang <= data.transfer.threshold)
+        {
+            biayaTransfer = data.transfer.low_fee;
+            totalBiaya = jumlahUang + biayaTransfer;
+        }
+        else if (jumlahUang > data.transfer.threshold)
+        {
+            biayaTransfer = data.transfer.high_fee;
+            totalBiaya = jumlahUang + biayaTransfer;
+        }
+
+        if (config.lang == "en")
+        {
+            Console.WriteLine($"Transfer Fee = " );
+        }
+        else if (config.lang == "id")
+        {
+            Console.Write("Masukkan jumlah uang yang akan di-transfer: ");
+        }
+    }
+}
